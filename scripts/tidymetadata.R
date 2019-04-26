b1ab287fb0dc0621cf43eb22bae56900ebe17d05
evaz <eva.zinkovsky@csiro.au>
2019-04-26 10:45:06 +1000

    separated ExternalID into DayLength and ID

--- scripts/tidymetadata.R
+++ scripts/tidymetadata.R
@@ -2,13 +2,13 @@
 library(tidyverse)
 
 # read in the metadata.csv file which is the renamed R_161128_SHADIL_LIB2500-M002.csv
-read_csv("metadata.csv")
+read_csv("data/metadata.csv")
 
 # remove the first 14 rows
-metadataless <- read_csv("metadata", skip = 14)
+metadataless <- read_csv("data/metadata.csv", skip = 14)
 
 # select columns called Sample/Name, Index and External ID
-mtdtlesscolloess <- select(metadataless, "Sample/Name" , "Index" , "External ID")
+mtdtlesscolless <- select(metadataless, "Sample/Name" , "Index" , "External ID")
 
 # rename the columns to remove "/" from the first column and " " from the third column
 names(mtdtlesscolless) <- c("SampleName" , "Index" , "ExternalID")
@@ -25,4 +25,8 @@ mtdtlesscollesssplit2 <- mtdtlesscollesssplitnonumbername %>%
   separate(Index, into = c("Index1" , "Index2" , "Index3" , "Index4" , "LeadSeq" , "TailSeq"))
 
 # select the columns with variable values and discard the columns which have the same thing in each cell
-mtdtlesscollesssplit2select <- mtdtlesscollesssplit2, "Sample" , "Index3" , "Index4" , "LeadSeq" , "TailSeq")
+mtdtlesscollesssplit2select.csv <- select(mtdtlesscollesssplit2, "Sample" , "Index3" , "Index4" , "LeadSeq" , "TailSeq" , "ExternalID")
+
+view(mtdtlesscollesssplit2select.csv)
+
+
